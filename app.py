247ffd8f83137d5107851d46ee8e6b905706fff2
Jhyna-Shady <71945183@est.unap.edu.pe>
2024-07-17 12:05:17 -0500

    Un comentario

---
+++ app.py
@@ -0,0 +1,4 @@
+# Importar las bibliotecas necesarias
+import streamlit as st  # Importa Streamlit para crear la interfaz web
+
+st.title("Gestión de Libros")  # Establece el título de la aplicación en la interfaz web
